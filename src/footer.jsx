6f854ae26991dcc89593d686c37249ae3598f757
Andrea Carmona <andrea@carmonaweb.com>
2023-09-22 12:21:59 -0400

    finished project

---
+++ src/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Footer component
+function Footer({ content }) {
+  return (
+    <footer>
+      <p>{content}</p>
+    </footer>
+  );
+}
+
+export default Footer;
